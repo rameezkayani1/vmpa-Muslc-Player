f58e87edad98cd059c92746b70b7120098355679
RAMEEZ KAYANI <RAMEEZKAYANI535@GMAIL.COM>
2023-12-03 15:44:52 +0500

    03122026

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:vmpa/tabs/songs.dart';
 
 import '../tabs/Album.dart';
@@ -17,81 +18,94 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            // backgroundColor: Colors.transparent,
-            elevation: 0,
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Image.asset("assets/logo.png",
-                  fit: BoxFit.cover, height: 20.00, width: 20.00),
-            ),
-            title: const Text(
-              "VMPA",
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-            ),
-            actions: [
-              Padding(
-                padding: EdgeInsets.only(right: 25),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    showSearch(
-                        context: context, delegate: MusicSearchDelegate());
-                  },
-                ),
+        child: Stack(children: [
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF4527A0),
+                  Color(0xFF1D1B29),
+                ],
               ),
-            ],
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  child: Text(
-                    "Song",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    "Album",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    "Artist",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Tab(
-                  child: Text(
-                    "playlist",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Image.asset("assets/logo.png",
+                    fit: BoxFit.cover, height: 20.00, width: 20.00),
+              ),
+              title: const Text(
+                "VMPA",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.only(right: 25),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Get.to(SearchPage());
+                    },
                   ),
                 ),
               ],
+              bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Song",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "Album",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "Artist",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "playlist",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            body: const TabBarView(
+              children: [
+                Songpage(),
+                MusicAlbum(),
+                Artist(),
+                MusicAlbum(),
+
+                // PlaylistPage(),
+
+                // PlaylistManagementPage(playlists: playlists,),
+                // playlist(),
+              ],
             ),
           ),
-          body: const TabBarView(
-            children: [
-              Songpage(),
-              MusicAlbum(),
-              Artist(),
-              MusicAlbum(),
-
-              // PlaylistPage(),
-
-              // PlaylistManagementPage(playlists: playlists,),
-              // playlist(),
-            ],
-          ),
-        ));
+        ]));
   }
 }
--- lib/tabs/search.dart
+++ lib/tabs/search.dart
@@ -1,93 +1,133 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
-import 'package:vmpa/Screens/playerScreen.dart';
+import 'package:get/get.dart';
 
+import '../Screens/playerScreen.dart';
 import '../widget/songscontoller.dart';
 
-class MusicSearchDelegate extends SearchDelegate<String> {
+class SearchPage extends StatelessWidget {
   final PlayerController controller = Get.put(PlayerController());
 
   @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF1D1B29), Color(0xFF4527A0)],
+          ),
+        ),
       ),
-    ];
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text("Search"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: () {
+                // Clear the search query
+                controller.audioQuery.querySongs(
+                  ignoreCase: true,
+                  orderType: OrderType.ASC_OR_SMALLER,
+                  sortType: null,
+                  uriType: UriType.EXTERNAL,
+                );
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: MusicSearchDelegate(controller: controller),
+        ),
+      ),
+    ]);
   }
+}
+
+class MusicSearchDelegate extends StatefulWidget {
+  final PlayerController controller;
+
+  MusicSearchDelegate({required this.controller});
 
   @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, '');
-      },
+  _MusicSearchDelegateState createState() => _MusicSearchDelegateState();
+}
+
+class _MusicSearchDelegateState extends State<MusicSearchDelegate> {
+  late Future<List<SongModel>> _searchResults;
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+    _searchResults = _loadAllSongs();
+  }
+
+  Future<List<SongModel>> _loadAllSongs() async {
+    return widget.controller.audioQuery.querySongs(
+      ignoreCase: true,
+      orderType: OrderType.ASC_OR_SMALLER,
+      sortType: null,
+      uriType: UriType.EXTERNAL,
     );
   }
 
   @override
-  Widget buildResults(BuildContext context) {
-    return SongSearchResults(query: query);
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return SongSearchResults(query: query);
-  }
-}
-
-class SongSearchResults extends StatelessWidget {
-  final String query;
-
-  const SongSearchResults({Key? key, required this.query}) : super(key: key);
-
-  @override
   Widget build(BuildContext context) {
-    final PlayerController controller = Get.find();
-
-    return FutureBuilder<List<SongModel>>(
-      future: controller.audioQuery.querySongs(
-        ignoreCase: true,
-        orderType: OrderType.ASC_OR_SMALLER,
-        sortType: null,
-        uriType: UriType.EXTERNAL,
-        //  where: 'title LIKE ? OR artist LIKE ? OR album LIKE ?',
-        //   whereArgs: ['%$query%', '%$query%', '%$query%'],
-      ),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData) {
-          List snap = snapshot.data;
-          if (snapshot.data == null) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.data!.isEmpty) {
-            return Center(
-              child: Text("No Song Found"),
-            );
-          } else {
-            return Padding(
-              padding: EdgeInsets.all(10.0),
-              child: ListView.builder(
-                itemCount: snap.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(32),
-                    ),
-                    child: ListTile(
+    return Column(
+      children: [
+        TextField(
+          style: TextStyle(fontSize: 20, color: Colors.white),
+          controller: _searchController,
+          onChanged: (query) {
+            // Update the search results as the user types
+            setState(() {
+              _searchResults = _filterSongs(query);
+            });
+          },
+          decoration: InputDecoration(
+            hintText: "Search for songs...",
+            hintStyle: TextStyle(fontSize: 18.0, color: Colors.white),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        FutureBuilder<List<SongModel>>(
+          future: _searchResults,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Error: ${snapshot.error}"));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(
+                  child: Text(
+                "No Song Found",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.white),
+              ));
+            } else {
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
                       title: Text(
                         "${snapshot.data![index].displayNameWOExt}",
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                          color: Colors.white,
-                        ),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.white),
                       ),
                       onTap: () {
                         Get.to(
@@ -96,7 +136,8 @@ class SongSearchResults extends StatelessWidget {
                           ),
                           transition: Transition.downToUp,
                         );
-                        controller.playSong(snapshot.data[index].uri, index);
+                        widget.controller
+                            .playSong(snapshot.data![index].uri, index);
                       },
                       subtitle: Text(
                         " ${snapshot.data![index].artist}",
@@ -107,30 +148,36 @@ class SongSearchResults extends StatelessWidget {
                         type: ArtworkType.AUDIO,
                         nullArtworkWidget: Icon(
                           Icons.music_note,
-                          color: Colors.white,
                           size: 32,
+                          color: Colors.white,
                         ),
                       ),
                       trailing: IconButton(
                         icon: Icon(
                           Icons.more_vert,
                           color: Colors.white,
-                          size: 32,
                         ),
                         onPressed: () {
-                          // Your code for handling more options...
+                          // Implement your actions here
                         },
                       ),
-                    ),
-                  );
-                },
-              ),
-            );
-          }
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
+                    );
+                  },
+                ),
+              );
+            }
+          },
+        ),
+      ],
     );
+  }
+
+  Future<List<SongModel>> _filterSongs(String query) async {
+    final List<SongModel> allSongs = await _loadAllSongs();
+    final List<SongModel> filteredSongs = allSongs
+        .where((song) =>
+            song.displayNameWOExt.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return filteredSongs;
   }
 }
